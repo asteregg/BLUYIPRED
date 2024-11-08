a5406ae8084fbc6aacf7b625259b9a9f62fe59c7
Santos Gervacio <santoshg17@outlook.com>
2024-11-08 16:56:57 -0500

    Prueba4

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 #SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower()=="true"
 
-# ALLOWED_HOSTS = ['bluyipred.onrender.com']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['bluyipred.onrender.com']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 RENDER_EXTERNA_HOSTNAME= os.environ.get('RENDER_EXTERNA_HOSTNAME')
