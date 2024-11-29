5d0c2d3e4ac5107391aed570622f5194efc4db1d
Santos Gervacio <santoshg17@outlook.com>
2024-11-29 02:03:11 -0500

    MI_BB_FAVORITO

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -22,7 +22,7 @@ import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-#SECRET_KEY = '6b1b2f61a0cb1985457bd60693ec0658'
+# SECRET_KEY = '6b1b2f61a0cb1985457bd60693ec0658'
 #SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower()=="true"
 
